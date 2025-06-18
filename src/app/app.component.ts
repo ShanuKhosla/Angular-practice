f07c8e99d88b75a05d7babfa37adf33652b73a46
Shantanu <shantanukhoslas@gmail.com>
2025-06-18 23:13:30 +0530

    Angular Event Listeners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-    <p> This is a simple Angular application.</p>
-    <router-outlet />
+    <app-header></app-header>
+    <main>
+      <app-home></app-home>
+    </main>
+    <router-outlet></router-outlet>
   `,
-  styles: [],
+  styles: [`
+    main {
+      padding: 20px;
+    }
+    `],
 })
 export class AppComponent {
   title = 'first-ng-app';
